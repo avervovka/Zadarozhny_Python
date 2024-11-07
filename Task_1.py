72355a8545e61f76289a3c526f8a95d9b62da81c
avervovka <avervovka@gmail.com>
2024-11-07 22:46:52 +0300

    All tasks

--- Task_1.py
+++ Task_1.py
@@ -1,12 +1,39 @@
-from array import *
+from types import NoneType
+import re
 
 
 def algo_print(a):
-    if isinstance(a, int):
+    print(a, type(a))
+    try:
+        if a.startswith('[') and a.endswith(']'):
+            a = list(a)
+            a_filtered = [i for i in a if is_valid(i)]
+            return a_filtered
+        elif a.startswith('{') or a.startswith('('):
+            return "Entered value is not valid"
+
+        a = float(a)
         if a > 7:
             return 'Hello'
-    elif isinstance(a, str):
-        return ['There is no such name', 'Hello, John'][a == 'John']
-    elif isinstance(a, array):
-        a_filtered = list(filter(lambda x: x % 3 == 0, a))
-        return a_filtered
+        return 'This number is not valid'
+    except ValueError:
+        if re.fullmatch(r'\w+', a):
+            return ['There is no such name', 'Hello, John'][a.lower() == 'john']
+        return 'Not Valid'
+    except Exception as e:
+        print(str(e))
+
+
+def is_valid(x):
+    try:
+        if not isinstance(x, (NoneType, bool, list, dict, tuple)):
+            float(x)
+            return True if float(x) % 3 == 0 else False
+    except ValueError:
+        return False
+
+
+if __name__ == "__main__":
+    print('Program Started')
+    print(algo_print(input()))
+    print('Program Finished')
